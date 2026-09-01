8db64cf8a0775151d06a09d657130dfffc0750ef
Ezra John <ezrakumo@gmail.com>
2026-09-01 11:51:13 +0100

    Add bridge migration to fix NULL IntegrityError before 0005

---
+++ insight/migrations/0004a_fill_ai_nulls.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+def fill_ai_nulls(apps, schema_editor):
+    # Get the historical version of the Report model
+    Report = apps.get_model('insight', 'Report')
+    
+    # Safely update all NULL values to empty strings so the next migration can run
+    Report.objects.filter(ai_suggested_category__isnull=True).update(ai_suggested_category='')
+    Report.objects.filter(ai_sentiment__isnull=True).update(ai_sentiment='')
+    Report.objects.filter(ai_urgency_level__isnull=True).update(ai_urgency_level='')
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('insight', '0004_remove_fieldagent_created_at_and_more'), 
+    ]
+
+    operations = [
+        migrations.RunPython(fill_ai_nulls),
+    ]

563a54629c0a2b147960aeb523f2dafb10ddb2cf
ezrakumo <ezrakumo@gmail.com>
2026-09-17 10:45:01 +0100

    Update ASGI config to include WebSocket routing
    Refactor ASGI configuration to support WebSocket connections.

--- tarabaintel/asgi.py
+++ tarabaintel/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for tarabaintel project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from insight.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tarabaintel.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # Handles normal HTTP requests (Web pages, API)
+    "http": get_asgi_application(),
+    
+    # Handles WebSocket connections (Real-time Map)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})

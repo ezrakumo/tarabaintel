b8141bc625e2f5d31143877a7449d1f705e62750
ezrakumo <ezrakumo@gmail.com>
2026-09-17 10:47:39 +0100

    Implement WebSocket consumer for intelligence feed

---
+++ insight/consumers.py
@@ -0,0 +1,29 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class IntelligenceConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'intelligence_feed'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+        print("✅ WebSocket Client Connected!")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        print("⚠️ WebSocket Client Disconnected")
+
+    async def receive(self, text_data):
+        pass # We only push from server to client
+
+    async def new_threat(self, event):
+        report = event['report']
+        await self.send(text_data=json.dumps({
+            'type': 'NEW_THREAT',
+            'report': report
+        }))
